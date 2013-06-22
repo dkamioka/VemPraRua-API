ea5edf7401c7da3915075893686485c3ed5b8cfd
Diogo Kamioka <diogo.key@mowa.com.br>
2013-06-22 02:26:29 -0300

    Adicionada a funcao que retorna is ids ativos na ultima X hora, e no controller nao adicionar nova location se nao tiver mudado suficientemente de lugar

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::UsersController < ApplicationController
+
   def create
     @user = User.create(params[:user])
     @user.name = "Anonymous \# #{@user.id}"
@@ -8,13 +9,33 @@ class Api::UsersController < ApplicationController
   def update
     if (@user = User.find_by_id(params[:id])) 
     @user.update_attributes(params[:user])
-    loc = Location.new
-    loc.latitude = params[:user][:latitude]
-    loc.longitude = params[:user][:longitude]
-    @user.locations << loc
-    @user.save
+
+=begin
+     Aqui comeca uma verificacao de latlong. Nao vai inserir uma nova location se nao tiver mudado o suficiente de lugar.
+     No caso, suficiente aqui foi 0.0002 de diferenca entre as latitudes e longitudes.
+     Existe um BUG, onde ja que eu faco diferencas entre modulos, se o cara apareceu no exato oposto,
+     digamos saiu de lat,long= -23,-46 para lat,long 23,46
+     Como a diferenca e calculada por modulos/absolutos ele nao detecta. 
+     Entretanto vou tratar como Know Issue. :P
+     diogo.kamioka
+=end
+
+    new_lat, new_long = params[:user][:latitude].to_f, params[:user][:longitude].to_f
+    old_loc = @user.locations.last
+      unless old_loc.nil?
+        old_lat, old_long = old_loc.latitude, old_loc.longitude
+      else 
+        old_lat, old_long = 0.0, 0.0
+      end
+
+        if ((new_lat.abs - old_lat.abs).abs + (new_long.abs - old_long.abs).abs > 0.0002 )
+          loc = Location.new
+          loc.latitude = new_lat
+          loc.longitude = new_long
+          @user.locations << loc
+          @user.save
+        end
     end
-    
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :fbuid, :latitude, :longitude, :name, :regid
   has_many :locations 
+
+  def last_online(horas)
+    last = [] 
+    User.where('updated_at > ?', Time.now - horas.hour).each { |x| last << x.id }
+    last
+  end
+
 end
